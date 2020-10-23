15fac6e7ffed84f7ea608c269571be91bb02c7dd
Veinstin Furtado <45301868+FurtadoDev@users.noreply.github.com>
2020-10-23 05:39:42 -0230

    Updated solution for container with most water problem

--- containerWithMostWater.cpp
+++ containerWithMostWater.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
+ 
+        int inputSize = height.size();
+        size_t leftIndex = 0;
+        size_t rightIndex = inputSize -1;
+        int tempArea = 0;
+        int maxArea = 0;
+        while(leftIndex != rightIndex){
+            if(rightIndex-leftIndex == 1){
+                tempArea = 1 * min(height[leftIndex], height[rightIndex]);
+                leftIndex++;
+            }else{
+                tempArea = (rightIndex-leftIndex) * min(height[leftIndex], height[rightIndex]);
+                if(height[leftIndex] < height[rightIndex])
+                    leftIndex++;
+                else
+                    rightIndex--;
+            } 
+            maxArea = max(maxArea, tempArea);
+        }
+        return maxArea;
     }
 };
